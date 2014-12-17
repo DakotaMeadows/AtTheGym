bea283d962df9cbff15d19df874e17f0d81df0f1
dakotameadows <dakota.w.meadows@gmail.com>
2014-12-17 11:41:42 -0500

    Completed workouts controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -4,4 +4,36 @@ class WorkoutsController < ApplicationController
     @workouts = Workout.all
   end
 
+  def show
+    @workout = Workout.find(params[:id])
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    if @workout.save
+      redirect_to workout_path(@workout)
+    else
+      redirect_to new_workout_path
+    end
+  end
+
+  def edit
+    @workout = Workout.find(params[:id])
+  end
+
+  def update
+    @workout = Workout.find(params[:id])
+    @workout.update_attibutes(post_params)
+    redirect_to workout_path(@workout)
+  end
+
+  def destroy
+    Workout.find(params[:id]).destroy
+    redirect_to workouts_path
+  end
+
 end
