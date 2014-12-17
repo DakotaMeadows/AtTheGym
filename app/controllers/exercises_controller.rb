786d4f12339e9ef8132428fb1aa3d9ae633c7893
dakotameadows <dakota.w.meadows@gmail.com>
2014-12-17 12:02:14 -0500

    Added form, edit, and show files to workout view

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,7 +1,39 @@
 class ExercisesController < ApplicationController
 
   def index
-    @exercises = Exercise.all
+    @exercises = exercise.all
+  end
+
+  def show
+    @exercise = exercise.find(params[:id])
+  end
+
+  def new
+    @exercise = exercise.new
+  end
+
+  def create
+    @exercise = exercise.new(exercise_params)
+    if @exercise.save
+      redirect_to exercise_path(@exercise)
+    else
+      redirect_to new_exercise_path
+    end
+  end
+
+  def edit
+    @exercise = exercise.find(params[:id])
+  end
+
+  def update
+    @exercise = exercise.find(params[:id])
+    @exercise.update_attibutes(post_params)
+    redirect_to exercise_path(@exercise)
+  end
+
+  def destroy
+    exercise.find(params[:id]).destroy
+    redirect_to exercises_path
   end
 
 end
